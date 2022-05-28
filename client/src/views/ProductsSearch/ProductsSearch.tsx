3cc9660c52fd752aa3365793cf5108cd3eca1f99
andrescuco <36432023+andrescuco@users.noreply.github.com>
2022-05-27 20:18:04 -0500

    Add bootstrap-grid to the searchbox component

--- client/src/views/ProductsSearch/ProductsSearch.tsx
+++ client/src/views/ProductsSearch/ProductsSearch.tsx
@@ -1,5 +1,6 @@
 import { useState, ChangeEvent } from "react";
 import { useNavigate, Outlet, Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 import logo from "../../../assets/logo.png";
 import searchIcon from "../../../assets/search-icon.png";
 import s from "./ProductsSearch.module.css";
@@ -18,21 +19,26 @@ function SearchBox() {
 
   return (
     <>
-      <div className={s.grid}>
-        <Link to="/">
-          <img src={logo} alt="meli logo" />
-        </Link>
-        <div className={s.searchBox}>
-          <input
-            type="text"
-            placeholder="Nunca dejes de buscar"
-            onChange={handleChange}
-          />
-          <button onClick={handleSubmit}>
-            <img src={searchIcon} alt="magnifying glass icon" />
-          </button>
-        </div>
-      </div>
+      <Container fluid className={s.container}>
+        <Row>
+          <Col md={{ span: 1, offset: 1 }}>
+            <Link to="/">
+              <img src={logo} alt="meli logo" />
+            </Link>
+          </Col>
+          <Col md={{ span: 9 }} className={s.searchBox}>
+            <input
+              type="text"
+              placeholder="Nunca dejes de buscar"
+              onChange={handleChange}
+            />
+            <button onClick={handleSubmit}>
+              <img src={searchIcon} alt="magnifying glass icon" />
+            </button>
+          </Col>
+        </Row>
+      </Container>
+
       <Outlet />
     </>
   );
